4278e55716e0157abd27423bd98693f582e3efad
Nasrin <64926916+nasrin55@users.noreply.github.com>
2023-11-02 14:16:12 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const imgContainer = document.querySelector('.img-container')
+const btnLeft = document.querySelector('.btn-left')
+const btnRight = document.querySelector('.btn-right')
+const images = document.querySelectorAll('.img-container  img')
+
+let currentSlide = 0
+let interval = setInterval(autorun, 3000)
+
+function autorun(){
+    currentSlide++
+    changeImage()
+}
+
+function changeImage(){
+    if(currentSlide > images.length - 1){
+        currentSlide = 0
+    }
+    else if(currentSlide < 0){
+        currentSlide = images.length - 1
+    }
+    imgContainer.style.transform = `translateX(-${currentSlide * imgContainer.offsetWidth}px)`
+}
+
+function resetInterval(){
+    clearInterval(interval)
+    interval = setInterval(autorun, 3000)
+}
+
+btnRight.addEventListener('click', function(){
+    currentSlide++
+    changeImage()
+    resetInterval()
+})
+
+btnLeft.addEventListener('click', function(){
+    currentSlide--
+    changeImage()
+    resetInterval()
+})
